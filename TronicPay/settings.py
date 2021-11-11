75e7eb633f69106c69e46540f16ee742192ef3f8
Taziamoma <taziamoma@gmail.com>
2021-11-11 12:23:17 -0600

    Initial setup

--- TronicPay/settings.py
+++ TronicPay/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
 
 SECRET_KEY = config("SECRET_KEY")
 
