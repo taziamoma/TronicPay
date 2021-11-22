6223cd5f4267438f22d801826ba110ef7a168435
Utsob Roy <roy@codesign.com.bd>
2021-11-22 10:50:55 +0600

    Strawberry graphql support

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 import tenant
+from strawberry.django.views import GraphQLView
+from api.schema import schema
 
 urlpatterns = [
+    path("graphql", GraphQLView.as_view(schema=schema)),
     path("login/", views.loginUser, name="login"),
     path("logout/", views.logoutUser, name="logout"),
     path("edit-profile/", views.EditProfileView, name="edit-profile"),
